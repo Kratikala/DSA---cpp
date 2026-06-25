1d2805662dcd874995e0bcc6ebffa26b5f9feeb8
Kratika Lekhra <kratikalekhra@gmail.com>
2026-06-25 18:49:36 +0530

    Problems

--- STL/1D_vectors_practice.cpp
+++ STL/1D_vectors_practice.cpp
@@ -62,61 +62,150 @@
 //     return 0;
 // }
 
-// 5. Count Even and Odd Numbers
+// // 5. Count Even and Odd Numbers
 
-// Example
+// # include <iostream>
+// # include <vector>
+// using namespace std;
+// int main(){
+//     vector <int> v = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+//     int even = 0;
+//     int odd = 0;
+//     for( int i : v){
+//         if( i % 2 == 0){
+//             even++;
+//         }
+//         else{
+//             odd++;
+//         }
+//     }
+//     cout << "EVEN : " << even << endl;
+//     cout << "ODD : " << odd << endl;
+//     return 0;
+// }
 
-// 1 2 3 4 5
+// // 6. Reverse a Vector
 
-// Output
+// # include <iostream>
+// # include <vector>
+// using namespace std;
+// int main(){
+//     vector <int> v = {1, 2, 3, 4, 5};
+//     int start = 0;
+//     int end = v.size() - 1;
+//     while (start < end){
+//         swap(v[start], v[end]);
+//         start++;
+//         end--;
+//     }
+//     for( int i : v){
+//         cout << i << " ";
+//     }
+//     return 0;
+// }
 
-// Even = 2
-// Odd = 3
-// 6. Reverse a Vector
+// // 7. Search an Element
 
-// Don't use STL reverse().
+// # include <iostream>
+// # include <vector>
+// using namespace std;
+// int main(){
+//     vector <int> v = {10, 20, 30, 40, 50};
+//     bool found = false;
+//     int target = 30;
+//     for( int i = 0; i < v.size(); i++){
+//         if (v[i] == target){
+//             cout << "Element is at index : " << i << " ";
+//             found = true;
+//             break;
+//         }
+//     }
+//     if(!found){
+//         cout << "Not Found" << endl;
+//     }
+//     return 0;
+// }
 
-// 7. Search an Element
+// // 8. Second Largest Element
 
-// Input
+// # include <iostream>
+// # include <vector>
+// using namespace std;
+// int main(){
+//     vector <int> v = {5, 9, 1, 4, 8};
+//     int largest = v[0];
+//     int second_largest = v[1];
+//     for( int i = 0; i < v.size(); i++){
+//         if (v[i] > largest){
+//             second_largest = largest;
+//             largest = v[i];
+//         }
+//         else if( v[i] > second_largest){
+//             second_largest = v[i];
+//         }
+//     }
+//     cout << "Second Largest Element is : " << second_largest << " ";
+//     return 0;
+// }
 
-// Vector: 10 20 30 40
-// Target = 30
+// // 9. Remove All Even Numbers
 
-// Output
+// # include <iostream>
+// # include <vector>
+// using namespace std;
+// int main(){
+//     vector <int> a = {1, 2, 3, 4, 5, 6, 7};
+//     vector <int> b;
+//     for(int i = 0; i < a.size(); i++){
+//         if( a[i] % 2 != 0){
+//             b.push_back(a[i]);
+//         }
+//     }
+//     for( int i : b){
+//         cout << i << " ";
+//     }
+//     return 0;
+// }
 
-// Found at index 2
-// 🟡 Level 2
-// 8. Second Largest Element
+// // 10. Rotate Vector Left by One
 
-// Example
+// # include <iostream>
+// # include <vector>
+// using namespace std;
+// int main(){
+//     vector <int> v = {1, 2, 3, 4, 5};
+//     int temp = v[0];
+//     int last = v.size() - 1;
+//     for( int i = 0; i < v.size(); i++){
+//         v[i] = v[i+1];
+//         v[last] = temp;
+//     }
+//     for( int i : v){
+//         cout << i << " ";
+//     }
+//     return 0;
+// }
 
-// 1 7 3 9 5
-
-// Output
-
-// 7
-// 9. Remove All Even Numbers
-
-// Input
-
-// 1 2 3 4 5 6
-
-// Output
-
-// 1 3 5
-// 10. Rotate Vector Left by One
-// 1 2 3 4 5
-
-// ↓
-
-// 2 3 4 5 1
 // 11. Rotate Right by One
-// 1 2 3 4 5
 
-// ↓
+# include <iostream>
+# include <vector>
+using namespace std;
+int main(){
+    vector <int> v = {1, 2, 3, 4, 5};
+    int last = v.size() - 1;
+    int first = v[0];
+    for(int i = v.size() - 1 ; i > 0; i--){
+        v[last] = first;
+        v[i] = v[i - 1];
 
-// 5 1 2 3 4
+    }
+    for(int i : v){
+        cout << i << " ";
+    }
+    return 0;
+}
+
 // 12. Check if Sorted
 
 // Return
