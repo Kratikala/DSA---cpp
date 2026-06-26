15c5997dec57cdca34dc69f225ab82a541fa3e07
Kratika Lekhra <kratikalekhra@gmail.com>
2026-06-26 21:44:57 +0530

    Queue Problems

---
+++ STL/queue_practice.cpp
@@ -0,0 +1,120 @@
+// 🟢 QUEUE (6 Questions)
+// 1. Insert
+
+// Insert
+
+// 10 20 30 40 50
+
+// Print
+
+// Front
+// Back
+// 2. Pop Two Elements
+
+// Print remaining queue.
+
+// 3. Print Queue
+
+// (Hint: Copy the queue.)
+
+// 4. Sum of Queue
+
+// Output
+
+// 150
+// 5. Count Even Numbers
+
+// Queue
+
+// 1 2 3 4 5 6
+
+// Output
+
+// 3
+// 6. Reverse Queue
+
+// (Hint: Stack + Queue.)
+
+// 🟡 DEQUE (5 Questions)
+// 1. Push Front
+
+// Insert
+
+// 10
+// 20
+// 30
+
+// using push_front()
+
+// 2. Push Back
+
+// Insert
+
+// 40
+// 50
+// 3. Pop Front
+
+// Print remaining deque.
+
+// 4. Pop Back
+
+// Print remaining deque.
+
+// 5. Print Deque
+
+// Traverse and print.
+
+// 🔴 PRIORITY QUEUE (6 Questions)
+// 1. Max Heap
+
+// Insert
+
+// 5 1 8 3 9 2
+
+// Print
+
+// 9 8 5 3 2 1
+// 2. Min Heap
+
+// Same numbers.
+
+// Output
+
+// 1 2 3 5 8 9
+// 3. Find Largest
+
+// Without sorting.
+
+// (Hint: top())
+
+// 4. Find Smallest
+
+// Using min heap.
+
+// 5. Print Priority Queue
+
+// Print all elements.
+
+// 6. K Largest
+
+// Insert
+
+// 10 4 15 2 20
+
+// Print first 3 largest.
+
+// Priority Queue
+
+// Find Kth Largest Element
+
+// Example
+
+// 3 2 1 5 6 4
+
+// Find
+
+// 2nd Largest
+
+// Answer
+
+// 5
