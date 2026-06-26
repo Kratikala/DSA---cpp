43528d04ad1ef7c9965dc6e0e4eaa955baec84e9
Kratika Lekhra <kratikalekhra@gmail.com>
2026-06-26 21:45:35 +0530

    Stack Problems

---
+++ STL/stack_practice.cpp
@@ -0,0 +1,68 @@
+// 🔵 STACK (6 Questions)
+// 1. Push 5 Elements
+
+// Print
+
+// Top:
+// Size:
+// 2. Pop 2 Elements
+
+// Print remaining top.
+
+// 3. Print All Elements
+
+// Print stack without changing the original stack.
+
+// (Hint: Make a copy.)
+
+// 4. Reverse a String
+
+// Input
+
+// Kratika
+
+// Output
+
+// akitarK
+
+// (Hint: Use stack.)
+
+// 5. Count Elements
+
+// Print
+
+// Size = 5
+// 6. Check Empty
+
+// After popping everything
+
+// Print
+
+// Empty
+
+// Stack
+
+// Balanced Parentheses
+
+// Input
+
+// ((()))
+
+// Output
+
+// Balanced
+// Queue
+
+// Generate Binary Numbers
+
+// Input
+
+// 5
+
+// Output
+
+// 1
+// 10
+// 11
+// 100
+// 101
