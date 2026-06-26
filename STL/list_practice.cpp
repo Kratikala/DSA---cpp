2fa258ed37830b439544302ff5a5ad5aa0b36d84
Kratika Lekhra <kratikalekhra@gmail.com>
2026-06-26 21:43:46 +0530

    List Problems

---
+++ STL/list_practice.cpp
@@ -0,0 +1,38 @@
+// 🟣 LIST (5 Questions)
+// 1. Print a List
+// list<int> l = {10,20,30,40,50};
+
+// Output:
+
+// 10 20 30 40 50
+// 2. Insert Elements
+// Insert 5 at front.
+// Insert 60 at back.
+
+// Output
+
+// 5 10 20 30 40 50 60
+// 3. Delete Elements
+
+// Remove
+
+// first element
+// last element
+// 4. Reverse a List
+
+// Input
+
+// 1 2 3 4 5
+
+// Output
+
+// 5 4 3 2 1
+
+// (Hint: Use STL function.)
+
+// 5. Find Sum of List
+
+// Output
+
+// 150
+
