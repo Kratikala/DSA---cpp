6405c5b75cd4439b63d54e3694fed7d76484a1ab
Kratika Lekhra <kratikalekhra@gmail.com>
2026-07-30 20:01:57 +0530

    Linked List Leetcode Problem

--- LINKED LIST/20_Intersection_Of_Two_LL.cpp
+++ LINKED LIST/20_Intersection_Of_Two_LL.cpp
@@ -1,3 +1,17 @@
+/*
+Topic      : Linked List
+Problem    : Intersection of Two Linked Lists
+Platform   : LeetCode 160 / Striver A2Z
+
+Approach   : Brute Force (Length Calculation & Aligning Longer List)
+Time       : O(N + M)
+Space      : O(1)
+
+Approach   : Optimal (Two Pointer Switching Technique)
+Time       : O(N + M)
+Space      : O(1)
+*/
+
 # include <iostream>
 # include <vector>
 using namespace std;
@@ -15,58 +29,65 @@ class Node
     }
 };
 
-// int LengthOfLL(Node* head)
-// {
-//     int count = 0;
-//     Node* temp = head;
-//     while(temp != NULL)
-//     {
-//         temp = temp->next;
-//         count++;
-//     }
-//     return count;
-// }
+// ---------- Length Function ----------
 
-// Node* Intersection(Node* head1, Node* head2)
-// {
-//     int len1 = LengthOfLL(head1);
-//     int len2 = LengthOfLL(head2);
+int LengthOfLL(Node* head)
+{
+    int count = 0;
+    Node* temp = head;
+    while(temp != NULL)
+    {
+        temp = temp->next;
+        count++;
+    }
+    return count;
+}
 
-//     Node* temp1 = head1;
-//     Node* temp2 = head2;
+// ---------- Approach 1 : Brute ----------
 
-//     if(len1 > len2)
-//     {
-//         int diff = len1 - len2;
+Node* IntersectionBrute(Node* head1, Node* head2)
+{
+    int len1 = LengthOfLL(head1);
+    int len2 = LengthOfLL(head2);
 
-//         for(int i = 0; i < diff; i++)
-//         {
-//             temp1 = temp1->next;
-//         }
-//     }
-//     else
-//     {
-//         int diff = len2 - len1;
+    Node* temp1 = head1;
+    Node* temp2 = head2;
 
-//         for(int i = 0; i < diff; i++)
-//         {
-//             temp2 = temp2 ->next;
-//         }
-//     }
+    if(len1 > len2)
+    {
+        int diff = len1 - len2;
 
-//     while(temp1 != NULL && temp2 != NULL)
-//     {
-//         if(temp1 == temp2)
-//         {
-//             return temp1;
-//         }
-//         temp1 = temp1->next;
-//         temp2 = temp2->next;  
-//     }
-//     return NULL;
-// }
+        for(int i = 0; i < diff; i++)
+        {
+            temp1 = temp1->next;
+        }
+    }
+    else
+    {
+        int diff = len2 - len1;
 
-Node* Intersection(Node* head1, Node* head2){
+        for(int i = 0; i < diff; i++)
+        {
+            temp2 = temp2 ->next;
+        }
+    }
+
+    while(temp1 != NULL && temp2 != NULL)
+    {
+        if(temp1 == temp2)
+        {
+            return temp1;
+        }
+        temp1 = temp1->next;
+        temp2 = temp2->next;  
+    }
+    return NULL;
+}
+
+// ---------- Approach 2 : Optimal ----------
+
+Node* IntersectionOptimal(Node* head1, Node* head2)
+{
     Node* A = head1;
     Node* B = head2;
     while(A != B)
@@ -106,7 +127,17 @@ int main()
     head2->next = new Node(6);
     head2->next->next = common;
 
-    Node* ans = Intersection(head1, head2);
+    Node* ans = IntersectionBrute(head1, head2);
+    if(ans != NULL)
+    {
+        cout << "Intersects at : " << ans->data << " ";
+    }
+    else
+    {
+        cout << "No Intersection takes place";
+    }
+
+    Node* ans = IntersectionOptimal(head1, head2);
     if(ans != NULL)
     {
         cout << "Intersects at : " << ans->data << " ";
