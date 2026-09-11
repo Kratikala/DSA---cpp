04914ea4b038d324f7decd0064436a7ab4c6528c
Kratika Lekhra <kratikalekhra@gmail.com>
2026-09-11 20:49:58 +0530

    Binary search

---
+++ BINARY SEARCH/14_Square_Of_Number.cpp
@@ -0,0 +1,48 @@
+/* 
+Topic : Binary Search 
+Problem : Square Root of a Number 
+
+Approach : Binary Search on Answer 
+Time : O(log n) 
+Space : O(1) 
+*/
+
+# include <iostream>
+# include <vector>
+using namespace std;
+
+int sqrt(int n)
+{
+    int low = 1;
+    int high = n;
+    int ans = -1;
+
+    while(low <= high)
+    {
+        int mid = low + (high - low) / 2;
+
+        if((mid * mid) <= n)
+        {
+            ans = mid;
+            low = mid + 1;
+        }
+        else
+        {
+            high = mid - 1;
+        }
+    }
+    return ans;
+}
+
+int main()
+{
+
+    int n;
+    cout << "Enter the root : ";
+    cin >> n;
+
+    int result = sqrt(n);
+    cout << "Square Root is : " << result << " ";
+    
+    return 0;
+}
